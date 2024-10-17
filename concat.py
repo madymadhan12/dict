16bc900ad66ecf815bd254140a105d85ba2d69a7
madymadhan12 <22hr1a3238@mtieat.org>
2024-10-17 14:22:03 +0530

    Create concat.py

---
+++ concat.py
@@ -0,0 +1,8 @@
+The program takes two dictionaries and concatenates them into one dictionary. 
+...
+a={"A":1,"B":2}
+b={"C":3}
+a.update(b)
+print("Concatenated dictionary is:",a)
+...
+Concatenated dictionary is: {'A': 1, 'B': 2, 'C': 3}
